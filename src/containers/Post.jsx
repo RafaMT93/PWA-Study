7ed86795af8e5e4c404d15ee5b72d67c06a9c927
RafaMT93 <rafat93.rdm@outlook.com.br>
2021-09-17 16:33:15 -0300

    Styles added

--- src/containers/Post.jsx
+++ src/containers/Post.jsx
@@ -4,7 +4,7 @@ import { Row, Col } from 'antd';
 import API from '../API';
 import { createMarkup } from '../utils';
 import Actions from './components/Actions';
-import styles from './Post.module.css';
+import './styles.css';
 
 const Post = () => {
   const { id, subject } = useParams();
@@ -74,14 +74,11 @@ const Post = () => {
           <h1 dangerouslySetInnerHTML={createMarkup(title)} />
           {image && renderImg({ image, description })}
           <p
-            className={styles.text}
+            className="text"
             dangerouslySetInnerHTML={createMarkup(description)}
           />
           <hr />
-          <p
-            className={styles.text}
-            dangerouslySetInnerHTML={createMarkup(body)}
-          />
+          <p className="text" dangerouslySetInnerHTML={createMarkup(body)} />
         </Col>
         <Col span={24} md={8}>
           <Row gutter={[16, 16]}>{news?.value?.map(renderPost)}</Row>
